459b57fea65edbe626e5f13b91c38f86886979ef
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-19 17:15:28 +0530

    refactor(main): Launch ChatBot GUI from main method - Entry point to open Swing GUI and initialize services

---
+++ AIChatBot/src/main/java/com/wearl/aichatbot/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wearl.aichatbot;
+
+/**
+ *
+ * @author Saurabh
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    ChatBotGUI.launchGUI();
+    TTSUtil.speak("Hello, I am your Java AI ChatBot speaking using FreeTTS!");
+}
+}
